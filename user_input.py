56077ffe9226f2a4300f47ff10811776c2f0af92
codewinchester <160033340+codewinchester@users.noreply.github.com>
2024-02-23 10:50:18 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+
+name = input("What's your name?")
+age = input ("How old are you?")
+location = input("Which area do you currently live in?")
+
+print("Hello" + " " + name + " " + "who is" + " " + age + "yrs old" + " " + "and stays at" + " " + location)
